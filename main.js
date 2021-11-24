e012e2f20eee04c3b929a1d37cd2fd88a012a2b0
Ami Fletcher <amifletcher@Amis-iMac.local>
2021-11-24 16:27:18 +0000

    started creating variables to select innerHTML

---
+++ main.js
@@ -0,0 +1,14 @@
+const taskAdd = document.querySelector(".taskAddBtn");
+const taskText = document.querySelector(".taskAdd");
+
+
+
+
+const selectTask = () => {
+    taskText.innerHTML
+}
+
+taskAdd.addEventListener("click", () => {
+     
+});
+
